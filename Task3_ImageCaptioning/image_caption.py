b9be7f1e369929d8804bcb5345df05fc5ff5f923
adityahdi12345-max <adityahdi12345@gmail.com>
2026-06-19 13:20:18 +0530

    Create image_caption.py

---
+++ Task3_ImageCaptioning/image_caption.py
@@ -0,0 +1,16 @@
+# Task 3 - Image Captioning AI
+# CodSoft AI Internship
+
+print("Image Captioning AI")
+
+# Sample image name
+image = "sample.jpg"
+
+# Generating caption
+print("Analyzing image...")
+
+# Simple caption output
+caption = "A beautiful image with objects and scenery."
+
+print("Generated Caption:")
+print(caption)
